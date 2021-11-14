d025773261226815237b232fb58f671aee0c3c1b
Zetka <jakub.kochanski10@gmail.com>
2021-11-14 19:11:13 +0100

    Dodanie klawiatury

--- kalkulator/lib/calculatorWidget.dart
+++ kalkulator/lib/calculatorWidget.dart
@@ -9,9 +9,160 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[800],
-      body: SafeArea(
-        child: Text('działa'),
+      backgroundColor: Colors.grey[700],
+      appBar: AppBar(
+        title: Text('kalkulator'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[900],
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              flex: 2,
+              child: Container(
+                child: Text('tu będzie wynik'),
+              )),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('1'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('2'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('3'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('+'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('4'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('5'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('6'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('-'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('7'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('8'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('9'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('x'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(','),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('0'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('='),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('/'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
