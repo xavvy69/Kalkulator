338df0a658a63caa2571f5c1a32d8a5ee0cbbc70
Zetka <jakub.kochanski10@gmail.com>
2021-11-14 17:54:21 +0100

    test

---
+++ kalkulator/lib/calculatorWidget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CalculatorWidget extends StatefulWidget {
+  @override
+  _CalculatorWidgetState createState() => _CalculatorWidgetState();
+}
+
+class _CalculatorWidgetState extends State<CalculatorWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: SafeArea(
+        child: Text('działa'),
+      ),
+    );
+  }
+}
